14662abb6738fecf1ad53db9b7f9527072262f6a
禹賢祐 <whw3312@gmail.com>
2021-12-24 19:13:01 +0900

    Toggle Study

---
+++ pages/toggle.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <v-btn @click="togglePopUp()">
+      click
+    </v-btn>
+    <div v-if="isToggle">
+      ひらき
+    </div>
+    <div v-else>
+      とじ
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      isToggle: false
+    }
+  },
+  computed: {
+  },
+  mounted () {
+  },
+  methods: {
+    togglePopUp () {
+      this.isToggle = !this.isToggle
+    }
+  }
+}
+</script>
